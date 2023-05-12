cb1fa5cfeca6c01f438ffeffe98591248a728fc2
GudwinAO <92263753+GudwinAO@users.noreply.github.com>
2023-05-12 16:00:03 +0400

    Lesson 2 (#2)
    * lesson_2_done

--- blog/app.py
+++ blog/app.py
@@ -1,18 +1,95 @@
 
-from flask import Flask
-from .views import index, greet_name, read_user, custom_status_code, power_value, do_zero_division, handle_zero_division_error, app
+from .views.users import users_app
+from .views.articles import articles_app
+
+from flask import Flask, request, g, render_template
+import time
+from werkzeug.exceptions import BadRequest
 
 
-VIEWS = [
-    index,
-    greet_name,
-    read_user,
-    custom_status_code,
-    power_value,
-    do_zero_division,
-    handle_zero_division_error
-]
+
+app = Flask(__name__)
+
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
+
 
 def create_app():
     return app
+    
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/greet/<name>/")
+def greet_name(name: str):
+        return f"Hello {name}!"
+
+
+@app.route("/user/")
+def read_user():
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return f"User {name or '[no name]'} {surname or '[no surname]'}"
+
+
+@app.route("/status/", methods=["GET", "POST"])
+def custom_status_code():
+    if request.method == "GET":
+        return """\
+                To get response with custom status code
+                send request using POST method
+                and pass `code` in JSON body / FormData
+                """
+    print("raw bytes data:", request.data)
+    
+    if request.form and "code" in request.form:
+        return "code from form", request.form["code"]
+
+    if request.json and "code" in request.json:
+        return "code from json", request.json["code"]
+    
+    return "", 204
+
+@app.route("/power/")
+def power_value():
+    x = request.args.get("x") or ""
+    y = request.args.get("y") or ""
+    if not (x.isdigit() and y.isdigit()):
+        app.logger.info("invalid values for power: x=%r and y=%r", x, y)
+        raise BadRequest("please pass integers in `x` and `y` query params")
+    
+    x = int(x)
+    y = int(y)
+    result = x ** y
+    app.logger.debug("%s ** %s = %s", x, y, result)
+    return str(f'Result={result}')
+
+@app.route("/divide-by-zero/")
+def do_zero_division():
+    return 1 / 0
+
+@app.errorhandler(ZeroDivisionError)
+def handle_zero_division_error(error):
+    print(error) # prints str version of error: 'division by zero'
+    app.logger.exception("Here's traceback for zero division error")
+    return "Never divide by zero!", 400
+
+
+# @app.before_request
+# def process_before_request():
+#     """
+#     #Sets start_time to `g` object
+#     """
+#     g.start_time = time()
+
+
+# @app.after_request
+# def process_after_request(response):
+#     """
+#     #adds process time in headers
+#     """
+#     if hasattr(g, "start_time"):
+#         response.headers["process-time"] = time()-g.start_time
+        
+#     return response 
